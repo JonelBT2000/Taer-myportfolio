8be11bca5ff77bb87961865ef6f3ac402736115e
JonelBT2000 <156985644+JonelBT2000@users.noreply.github.com>
2024-01-18 13:25:02 +0800

    Add files via upload

---
+++ src/components/header/HeaderSocials.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { BsLinkedin } from "react-icons/bs";
+import { FaGithub } from "react-icons/fa";
+
+const HeaderSocials = () => {
+  return (
+    <div className="header__socials">
+      <a
+        href="https://github.com/JonelBT2000"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <BsLinkedin />
+      </a>
+      <a
+        href="https://github.com/JonelBT2000"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <FaGithub />
+      </a>
+    </div>
+  );
+};
+
+export default HeaderSocials;
